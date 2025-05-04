b6c7765bc7c1cd2f29114b36bda1c7ea98c8f1b3
Alexsandro Oliveira <alexsandrolt1996@gmail.com>
2025-05-04 16:11:23 -0300

    petshop

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 const form = document.querySelector(".formulario-fale-conosco");
 
 function mostrarForm() {
-  form.style.left = "50%";
-  form.style.transform = "translateX(-50%)";
-  document.body.style.overflow = "hidden"; 
+  form.style.display = "flex"; // 🔵 Mostra o formulário
+  document.body.style.overflow = "hidden";
 }
 
 function esconderForm() {
-  form.style.left = "-300px"; 
-  form.style.transform = "translateX(0)";
-  document.body.style.overflow = "auto"; 
+  form.style.display = "none"; // 🔴 Esconde o formulário
+  document.body.style.overflow = "auto";
+}
 
 const fecharContato = document.querySelector(".fechar-contato");
 fecharContato.addEventListener("click", esconderForm);
-}
+
